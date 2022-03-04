dfdab9eae4f4a6e15d6d95095627370bf9f7635f
Nareshbhai Mahto <82165555+NareshMahto@users.noreply.github.com>
2022-03-05 00:12:40 +0530

    Create Factors.cpp

---
+++ Conditionals and Loops/Assignment/Factors.cpp
@@ -0,0 +1,41 @@
+/*
+Factors
+---------------------------------------
+Write a program to print all the factors of a number other than 1 and the number itself.
+
+Input Format :
+A single integer, n
+
+Output Format :
+All the factors of n excluding 1 and the number itself
+
+Constraints :
+0 <= n <= 10,000
+
+Sample Input 1 :
+8
+
+Sample Output 1 :
+2 4
+
+Sample Input 2 :
+11
+
+Sample Output 2 :
+
+Explanation of Sample Output 2 :
+No output as 11 is prime having factors as 1 and 11 only
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i;
+    cin >> n;
+    for(i = 2; i < n; ++i) {
+        if(n % i == 0)
+            cout << i << " ";
+    }
+    return 0;
+}
